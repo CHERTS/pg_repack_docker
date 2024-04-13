a157d82591f3d273a7a47f39b700c7d14a26a979
Mikhail Grigorev <sleuthhound@gmail.com>
2024-04-13 22:59:57 +0500

    Fixed

--- build.sh
+++ build.sh
@@ -8,6 +8,18 @@ PG_REPACK=(
     "16,1.5.0"
 )
 
+# Check command exist function
+_command_exists() {
+    type "$1" &> /dev/null
+}
+
+if _command_exists docker; then
+    DOCKER_BIN=$(which docker)
+else
+    echo "ERROR: Command 'docker' not found."
+    exit 1
+fi
+
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
 
@@ -16,17 +28,17 @@ function ctrl_c() {
     exit 1
 }
 
-#docker login -u XXXXX -p YYYYY
+#${DOCKER_BIN} login -u XXXXX -p YYYYY
 
 for DATA in ${PG_REPACK[@]}; do
     PG_VER=$(echo "${DATA}" | awk -F',' '{print $1}')
     PGREPACK_VER=$(echo "${DATA}" | awk -F',' '{print $2}')
     echo "Docker build image 'pg-repack:${PGREPACK_VER}', postgres v${PG_VER}..."
-    docker build -t cherts/pg-repack:${PGREPACK_VER} --no-cache --progress=plain -f ${PG_VER}/Dockerfile ${PG_VER}
+    ${DOCKER_BIN} build -t cherts/pg-repack:${PGREPACK_VER} --no-cache --progress=plain -f ${PG_VER}/Dockerfile ${PG_VER}
     if [ $? -eq 0 ]; then
 	echo "Done build image."
         #echo "Docker push image 'pg-repack:${PGREPACK_VER}'..."
-        #docker push cherts/pg-repack:${PGREPACK_VER}
+        #${DOCKER_BIN} push cherts/pg-repack:${PGREPACK_VER}
     else
         echo "ERROR: Failed build image 'pg-repack:${PGREPACK_VER}', postgres v${PG_VER}"
         exit 1
